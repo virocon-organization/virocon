0de128aa16b4897faa03c56ecc0aac662ceb8f87
kawindme <kawindme@uni-bremen.de>
2018-04-09 19:09:17 +0200

    try to add matplotlib instead of plt to namespace in fixture

--- viroconcom/conftest.py
+++ viroconcom/conftest.py
@@ -6,7 +6,6 @@ all_backends = matplotlib.rcsetup.all_backends
 backend_worked = False
 for gui in all_backends:
     try:
-        print("Testing", gui)
         matplotlib.use(gui, warn=False, force=True)
         from matplotlib import pyplot as plt
         backend_worked = True
@@ -16,4 +15,4 @@ for gui in all_backends:
 
 @pytest.fixture(autouse=True, scope="session")
 def add_np(doctest_namespace):
-    doctest_namespace['plt'] = plt
+    doctest_namespace['matplotlib'] = matplotlib
