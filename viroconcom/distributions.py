8b114e55e683deaca2bef008daccec9a86a8b147
Andreas <a.haselsteiner@uni-bremen.de>
2018-05-14 19:02:08 +0200

    Use double backslash everywhere, #18

--- viroconcom/distributions.py
+++ viroconcom/distributions.py
@@ -85,22 +85,22 @@ class ParametricDistribution(Distribution, ABC):
                         | github.io/viroconcom/_images/ | shape = sigma
                         | pdf_log_normal.png            | location = -
         :math:`f(x) = \\frac{1}{\\sqrt{2 \\pi \\sigma^2}} \\exp\\left[{-\\frac{(x-\\mu)^2}{2\\sigma^2}}\\right]`
-        :math:`f(x) = \frac{\beta}{\alpha}\left( \frac{x-\gamma}{\alpha}\right)^{\beta -1} \exp \left[-\left( \frac{x-\gamma}{\alpha} \right)^{\beta} \right]`
-        :math:`f(x) = \frac{1}{x\widetilde{\sigma} \sqrt{2\pi}}\exp \left[ \frac{-(\ln x - \widetilde{\mu})^2}{2\widetilde{\sigma}^2}\right]$ & scale: $e^{\widetilde{\mu}}`
+        :math:`f(x) = \\frac{\\beta}{\\alpha}\\left( \\frac{x-\\gamma}{\\alpha}\\right)^{\\beta -1} \\exp \\left[-\\left( \\frac{x-\\gamma}{\\alpha} \\right)^{\\beta} \\right]`
+        :math:`f(x) = \\frac{1}{x\\widetilde{\\sigma} \\sqrt{2\\pi}}\\exp \\left[ \\frac{-(\\ln x - \\widetilde{\\mu})^2}{2\\widetilde{\\sigma}^2}\\right]$ & scale: $e^{\\widetilde{\\mu}}`
 
-        ============  ====================================================================================================================================================================  =====================
-        distribution  probability density function                                                                                                                                          statistical parameter
-        ============  ====================================================================================================================================================================  =====================
-        normal        :math:`f(x) = \frac{1}{\sqrt{2 \pi \sigma^2}} \exp\left[{-\frac{(x-\mu)^2}{2\sigma^2}}\right]`                                                                        scale = sigma
-                                                                                                                                                                                            shape = -
-                                                                                                                                                                                            loc = mu
-        Weibull       :math:`f(x) = \frac{\beta}{\alpha}\left( \frac{x-\gamma}{\alpha}\right)^{\beta -1} \exp \left[-\left( \frac{x-\gamma}{\alpha} \right)^{\beta} \right]`                scale = alpha
-                                                                                                                                                                                            shape = beta
-                                                                                                                                                                                            loc = gamma
-        log-normal    :math:`f(x) = \frac{1}{x\widetilde{\sigma} \sqrt{2\pi}}\exp \left[ \frac{-(\ln x - \widetilde{\mu})^2}{2\widetilde{\sigma}^2}\right]$ & scale: $e^{\widetilde{\mu}}`  scale = e^mu
-                                                                                                                                                                                            shape = sigma
-                                                                                                                                                                                            loc = -
-        ============  ====================================================================================================================================================================  =====================
+        ============  ====================================================================================================================================================================================  =====================
+        distribution  probability density function                                                                                                                                                          statistical parameter
+        ============  ====================================================================================================================================================================================  =====================
+        normal        :math:`f(x) = \\frac{1}{\\sqrt{2 \\pi \\sigma^2}} \\exp\\left[{-\\frac{(x-\\mu)^2}{2\\sigma^2}}\\right]`                                                                              scale = sigma
+                                                                                                                                                                                                            shape = -
+                                                                                                                                                                                                            loc = mu
+        Weibull       :math:`f(x) = \\frac{\\beta}{\\alpha}\\left( \\frac{x-\\gamma}{\\alpha}\\right)^{\\beta -1} \\exp \\left[-\\left( \\frac{x-\\gamma}{\\alpha} \\right)^{\\beta} \\right]`              scale = alpha
+                                                                                                                                                                                                            shape = beta
+                                                                                                                                                                                                            loc = gamma
+        log-normal    :math:`f(x) = \\frac{1}{x\\widetilde{\\sigma} \\sqrt{2\\pi}}\\exp \\left[ \\frac{-(\\ln x - \\widetilde{\\mu})^2}{2\\widetilde{\\sigma}^2}\\right]$ & scale: $e^{\\widetilde{\\mu}}`  scale = e^mu
+                                                                                                                                                                                                            shape = sigma
+                                                                                                                                                                                                            loc = -
+        ============  ====================================================================================================================================================================================  =====================
 
         Parameters
         ----------
