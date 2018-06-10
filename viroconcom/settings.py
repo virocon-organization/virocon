2a8d6e1db9dee78d21a8fb953ee537ebde47dec9
ahaselsteiner <a.haselsteiner@uni-bremen.de>
2018-06-10 17:35:19 +0200

    Make version accessible as constant in the settings.py file, increase version number for pypi

--- viroconcom/settings.py
+++ viroconcom/settings.py
@@ -1,3 +1,9 @@
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'VERSION')) as version_file:
+    VERSION = version_file.read().strip()
+
 SHAPE_STRING = "shape"
 LOCATION_STRING = "loc"
 SCALE_STRING = "scale"
