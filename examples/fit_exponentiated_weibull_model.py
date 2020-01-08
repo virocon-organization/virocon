97035ee3f6d3eb1ecca2b0dcde11143dc9d2f1eb
Andreas <a.haselsteiner@uni-bremen.de>
2020-01-08 11:32:17 +0100

    Compute contour based on exponentiated Weibull distribution model in example.

--- examples/fit_exponentiated_weibull_model.py
+++ examples/fit_exponentiated_weibull_model.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from viroconcom.fitting import Fit
-from viroconcom.contours import IFormContour
+from viroconcom.contours import HighestDensityContour
 
 
 def read_dataset(path='examples/datasets/A.txt'):
@@ -61,9 +61,27 @@ dist_description_tz = {'name': 'Lognormal_SigmaMu',
 # Fit the model to the data.
 fit = Fit((sample_hs, sample_tz), (
     dist_description_hs, dist_description_tz))
+mul_dist = fit.mul_var_dist
 dist0 = fit.mul_var_dist.distributions[0]
 print('First variable: ' + dist0.name + ' with '
       + ' scale: ' + str(dist0.scale) + ', '
       + ' shape: ' + str(dist0.shape) + ', '
       + ' location: ' + str(dist0.loc))
 print('Second variable: ' + str(fit.mul_var_dist.distributions[1]))
+
+
+# Compute an IFORM contour with a return period of 25 years, a sea state
+# duration of 3 hours and 100 points along the contour.
+tr = 50 # Return period in years.
+ts = 1 # Sea state duration in hours.
+limits = [(0, 20), (0, 20)] # Limits of the computational domain.
+deltas = [0.01, 0.01] # Dimensions of the grid cells.
+hdens_contour = HighestDensityContour(mul_dist, tr, ts, limits, deltas)
+
+# Plot the contour.
+plt.scatter(hdens_contour.coordinates[0][0], hdens_contour.coordinates[0][1])
+plt.xlabel('zero-up-crossing period [s]')
+plt.ylabel('significant wave height (m)')
+
+plt.legend()
+plt.show()
