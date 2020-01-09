752af01dfcbacc07436489afefba1458fea50389
Andreas <a.haselsteiner@uni-bremen.de>
2020-01-09 16:48:57 +0100

    Implement lnsquare2 depdence function and a test for it.

--- tests/test_fitting.py
+++ tests/test_fitting.py
@@ -1,8 +1,51 @@
 import unittest
-
+import csv
 import numpy as np
 
 from viroconcom.fitting import Fit
+
+
+def read_dataset(path='tests/testfiles/1year_dataset_A.txt'):
+    """
+    Reads a datasets provided for the environmental contour benchmark.
+    Parameters
+    ----------
+    path : string
+        Path to dataset including the file name, defaults to 'examples/datasets/A.txt'
+    Returns
+    -------
+    x : ndarray of doubles
+        Observations of the environmental variable 1.
+    y : ndarray of doubles
+        Observations of the environmental variable 2.
+    x_label : str
+        Label of the environmantal variable 1.
+    y_label : str
+        Label of the environmental variable 2.
+    """
+
+    x = list()
+    y = list()
+    x_label = None
+    y_label = None
+    with open(path, newline='') as csv_file:
+        reader = csv.reader(csv_file, delimiter=';')
+        idx = 0
+        for row in reader:
+            if idx == 0:
+                x_label = row[1][
+                          1:]  # Ignore first char (is a white space).
+                y_label = row[2][
+                          1:]  # Ignore first char (is a white space).
+            if idx > 0:  # Ignore the header
+                x.append(float(row[1]))
+                y.append(float(row[2]))
+            idx = idx + 1
+
+    x = np.asarray(x)
+    y = np.asarray(y)
+    return (x, y, x_label, y_label)
+
 
 class FittingTest(unittest.TestCase):
 
@@ -100,6 +143,43 @@ class FittingTest(unittest.TestCase):
         self.assertLess(dist0.scale(0), 4)
         self.assertGreater(dist0.shape2(0), 0.5) # Should be about 1.
         self.assertLess(dist0.shape2(0), 2)
+
+
+    def test_fit_lnsquare2(self):
+        """
+        Tests a 2D fit that includes an logarithm square dependence function.
+        """
+
+        sample_hs, sample_tz, label_hs, label_tz = read_dataset()
+
+
+        # Define the structure of the probabilistic model that will be fitted to the
+        # dataset.
+        dist_description_hs = {'name': 'Weibull_Exp',
+                               'dependency': (None, None, None, None),
+                               # Shape, Location, Scale, Shape2
+                               'width_of_intervals': 0.5}
+        dist_description_tz = {'name': 'Lognormal_SigmaMu',
+                               'dependency': (0, None, 0),
+                               # Shape, Location, Scale
+                               'functions': ('exp3', None, 'lnsquare2')
+                               # Shape, Location, Scale
+                               }
+
+        # Fit the model to the data, first test a 1D fit.
+        fit = Fit(sample_hs, dist_description_hs)
+        # Now perform the 2D fit.
+        fit = Fit((sample_hs, sample_tz),
+                  (dist_description_hs, dist_description_tz))
+
+        dist0 = fit.mul_var_dist.distributions[0]
+        dist1 = fit.mul_var_dist.distributions[1]
+
+        self.assertGreater(dist1.scale.a, 1) # Should be about 1-5
+        self.assertLess(dist1.scale.a, 5)  # Should be about 1-5
+        self.assertGreater(dist1.scale.b, 2) # Should be about 2-10
+        self.assertLess(dist1.scale.b, 10)  # Should be about 2-10
+
 
     def test_multi_processing(selfs):
         """
