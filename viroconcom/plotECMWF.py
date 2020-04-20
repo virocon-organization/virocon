b0629f6a89ffcc5d40b87a3f9f05c2fb362e6721
lbekov <lbekov@uni-bremen.de>
2020-04-20 11:13:04 +0200

    aufräumen

--- viroconcom/plotECMWF.py
+++
@@ -1,6 +0,0 @@
-import pygrib
-import matplotlib.pyplot as plt
-import matplotlib.colors as colors
-from mpl_toolkits.basemap import Basemap
-from mpl_toolkits.basemap import shiftgrid
-import numpy as np
