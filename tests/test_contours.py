7266a8385068172759f08c7eda89c15b7050658f
Andreas <a.haselsteiner@uni-bremen.de>
2020-07-28 00:16:58 +0200

    Simplify contour coordinates data structure.

--- tests/test_contours.py
+++ tests/test_contours.py
@@ -67,8 +67,8 @@ class ContourCreationTest(unittest.TestCase):
         test_contour_HDC = HighestDensityContour(mul_dist, n_years, 3,
                                                  limits, deltas)
 
-        finaldt0 = pd.DataFrame({'x' : test_contour_HDC.coordinates[0][0],
-                                'y' : test_contour_HDC.coordinates[0][1]})
+        finaldt0 = pd.DataFrame({'x' : test_contour_HDC.coordinates[0],
+                                'y' : test_contour_HDC.coordinates[1]})
 
 
         result0 = pd.read_csv(testfiles_path + "/HDC2dWL_coordinates.csv")
@@ -168,9 +168,9 @@ class ContourCreationTest(unittest.TestCase):
 
         # Compare the computed contours to the contours published in
         # 'Global hierarchical models for wind and wave contours', Figure 8.
-        max_v = max(test_contour_HDC.coordinates[0][0])
+        max_v = max(test_contour_HDC.coordinates[0])
         self.assertAlmostEqual(max_v, 29.5, delta=0.5) # Should be about 29.5
-        max_hs = max(test_contour_HDC.coordinates[0][1])
+        max_hs = max(test_contour_HDC.coordinates[1])
         self.assertAlmostEqual(max_hs, 14.5, delta=0.5) # Should be about 15
 
 
@@ -215,9 +215,9 @@ class ContourCreationTest(unittest.TestCase):
         test_contour_HDC = HighestDensityContour(mul_dist, n_years, 3,
                                                  limits, deltas)
 
-        finaldt = pd.DataFrame({'x' : test_contour_HDC.coordinates[0][0],
-                                'y' : test_contour_HDC.coordinates[0][1],
-                                'z' : test_contour_HDC.coordinates[0][2]})
+        finaldt = pd.DataFrame({'x' : test_contour_HDC.coordinates[0],
+                                'y' : test_contour_HDC.coordinates[1],
+                                'z' : test_contour_HDC.coordinates[2]})
 
 
         result = pd.read_csv(testfiles_path + "/HDC3dWLL_coordinates.csv")
@@ -305,8 +305,8 @@ class ContourCreationTest(unittest.TestCase):
         test_contour_HDC = HighestDensityContour(mul_dist, n_years, 3,
                                                  limits, deltas)
 
-        finaldt2 = pd.DataFrame({'x' : test_contour_HDC.coordinates[0][0],
-                                 'y' : test_contour_HDC.coordinates[0][1]})
+        finaldt2 = pd.DataFrame({'x' : test_contour_HDC.coordinates[0],
+                                 'y' : test_contour_HDC.coordinates[1]})
 
         result2 = pd.read_csv(testfiles_path + "/HDC2dWN_coordinates.csv")
 
@@ -354,9 +354,9 @@ class ContourCreationTest(unittest.TestCase):
         test_contour_HDC = HighestDensityContour(mul_dist, n_years, 3,
                                                  limits, deltas)
 
-        finaldt3 = pd.DataFrame({'x' : test_contour_HDC.coordinates[0][0],
-                                 'y' : test_contour_HDC.coordinates[0][1],
-                                 'z' : test_contour_HDC.coordinates[0][2]})
+        finaldt3 = pd.DataFrame({'x' : test_contour_HDC.coordinates[0],
+                                 'y' : test_contour_HDC.coordinates[1],
+                                 'z' : test_contour_HDC.coordinates[2]})
 
         matlab3 = pd.read_csv(testfiles_path + "/hdc3d_wln.csv", names=['x', 'y', 'z'])
 
@@ -396,8 +396,8 @@ class ContourCreationTest(unittest.TestCase):
 
         test_contour_IForm = IFormContour(mul_dist, 50, 3, 50)
 
-        calculated_coordinates = pd.DataFrame({'x' : test_contour_IForm.coordinates[0][0],
-                                 'y' : test_contour_IForm.coordinates[0][1]})
+        calculated_coordinates = pd.DataFrame({'x' : test_contour_IForm.coordinates[0],
+                                 'y' : test_contour_IForm.coordinates[1]})
         #calculated_coordinates.to_csv('save_this_file.csv', sep=',', header=['x', 'y'], index=False)
 
         true_coordinates = pd.read_csv(testfiles_path + "/IForm2dWL_coordinates.csv")
@@ -435,8 +435,8 @@ class ContourCreationTest(unittest.TestCase):
 
         test_contour_IForm = IFormContour(mul_dist, 50, 3, 50)
 
-        calculated_coordinates = pd.DataFrame({'x' : test_contour_IForm.coordinates[0][0],
-                                 'y' : test_contour_IForm.coordinates[0][1]})
+        calculated_coordinates = pd.DataFrame({'x' : test_contour_IForm.coordinates[0],
+                                 'y' : test_contour_IForm.coordinates[1]})
 
         true_coordinates = pd.read_csv(testfiles_path + "/IForm2dWN_coordinates.csv")
 
@@ -462,8 +462,6 @@ class ContourCreationTest(unittest.TestCase):
         mu = FunctionParam('power3', 0.1000, 1.489, 0.1901)
         sigma = FunctionParam('exp3', 0.0400, 0.1748, -0.2243)
 
-        #del shape, loc, scale
-
         # Create distributions.
         dist1 = WeibullDistribution(*par1)
         dist2 = LognormalDistribution(mu=mu, sigma=sigma)
@@ -473,7 +471,7 @@ class ContourCreationTest(unittest.TestCase):
 
         mul_dist = MultivariateDistribution(distributions, dependencies)
 
-        test_contour_IForm = IFormContour(mul_dist, 50, 3, 400)
+        test_contour_iform = IFormContour(mul_dist, 50, 3, 400)
 
     def test_isorm2d_WL(self):
         """
@@ -507,8 +505,8 @@ class ContourCreationTest(unittest.TestCase):
 
         test_contour_isorm = ISormContour(mul_dist, 50, 3, 50)
 
-        calculated_coordinates = pd.DataFrame({'x': test_contour_isorm.coordinates[0][0],
-                                               'y': test_contour_isorm.coordinates[0][1]})
+        calculated_coordinates = pd.DataFrame({'x': test_contour_isorm.coordinates[0],
+                                               'y': test_contour_isorm.coordinates[1]})
 
         true_coordinates = pd.read_csv(testfiles_path + "/isorm2dWL_coordinates.csv")
         for o, p in [(o, p) for o in true_coordinates.index for p in true_coordinates.columns]:
