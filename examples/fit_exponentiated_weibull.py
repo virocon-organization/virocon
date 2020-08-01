1047705b05d88a87f33fd21a02cb3268cc2311a9
Andreas <a.haselsteiner@uni-bremen.de>
2020-08-02 00:57:43 +0200

    Add comments to example.

--- examples/fit_exponentiated_weibull.py
+++ examples/fit_exponentiated_weibull.py
@@ -1,19 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from viroconcom.params import ConstantParam
 from viroconcom.distributions import ExponentiatedWeibullDistribution
 from viroconcom.read_write import read_ecbenchmark_dataset
 
+# Load sea state measurements from the NDBC buoy 44007.
 hs, tz, label_hs, label_tz = \
     read_ecbenchmark_dataset('datasets/1year_dataset_A.txt')
 
-dist = ExponentiatedWeibullDistribution(shape=ConstantParam(0.6), scale=ConstantParam(3), shape2=ConstantParam(4))
-#params = dist.fit(hs)
+# Fit the exponentiated Weibull distribution to the measurements.
+dist = ExponentiatedWeibullDistribution()
+params = dist.fit(hs)
+
+# Print the distribution object, which outputs the estimated parameters.
 print(dist)
 
+# Plot the density of the empirical distribution and the fitted distribution.
 fig, ax = plt.subplots(1, 1)
-plt.hist(hs, density=True, label='Empirical distribution')
+plt.hist(hs, bins=20, density=True, label='Empirical distribution')
 x = np.linspace(0, max(hs), 200)
 plt.plot(x, dist.pdf(x), label='Fitted distribution')
 plt.xlabel(label_hs)
