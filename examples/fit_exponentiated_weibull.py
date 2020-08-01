8d4aac81c42f9f59013ea9ec88846a20f0b1624c
Andreas <a.haselsteiner@uni-bremen.de>
2020-08-01 23:15:34 +0200

    Example for fitting the exponentiated Weibull distribution.

---
+++ examples/fit_exponentiated_weibull.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from viroconcom.distributions import ExponentiatedWeibullDistribution
+from viroconcom.read_write import read_ecbenchmark_dataset
+
+hs, tz, label_hs, label_tz = \
+    read_ecbenchmark_dataset('datasets/1year_dataset_A.txt')
+
+dist = ExponentiatedWeibullDistribution()
+params = dist.fit(hs)
+print(dist)
+
+fig, ax = plt.subplots(1, 1)
+plt.hist(hs, density=True, label='Empirical distribution')
+x = np.linspace(0, max(hs), 200)
+plt.plot(x, dist.pdf(x), label='Fitted distribution')
+plt.xlabel(label_hs)
+plt.ylabel('Probability density (-)')
+ax.legend()
+plt.show()
