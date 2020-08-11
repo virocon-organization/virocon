bab498693839d8b3660c2bdb5b04a1dcb86277ab
kawindme <32780200+kawindme@users.noreply.github.com>
2020-08-11 15:53:02 +0200

    create PCA class and add tests for PCA

---
+++ tests/test_pca.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+import numpy as np
+import pandas as pd
+
+from numpy.testing import assert_allclose
+
+from .context import viroconcom
+
+from viroconcom.utility import PCA
+from viroconcom.fitting import Fit
+from viroconcom.contours import IFormContour
+
+@pytest.mark.parametrize("buoy_num", [46022, 46023, 46025, 46050])
+#@pytest.mark.parametrize("buoy_num", [46022])
+def test_IFORM_contour_with_PCA_like_EckertGallupp2016(buoy_num):
+    # according to Eckert-Gallup2016: https://doi.org/10.1016/j.oceaneng.2015.12.018.
+    # and https://github.com/WEC-Sim/WDRT/blob/master/WDRT/ESSC.py
+    buoy_data = pd.read_csv(f"tests/testfiles/NDBC_buoy_{buoy_num}.csv")
+    pca = PCA(buoy_data[["Hs", "T"]].values)
+    Comp1_Comp2 = pca.transformed
+    
+    Time_SS = 1  # Sea state duration (hrs)
+    Time_R = 100  # Return periods (yrs) of interest
+    
+    dist_description_0 = {"name": "InverseGaussian",
+                          "dependency": (None, None, None),
+                          "samples_per_intervals": 250,
+                          }
+    dist_description_1 = {"name": "Normal", 
+                          "dependency": (None, 0, 0),
+                          "functions": (None, "poly1", "poly2"),
+                          }
+
+    my_fit = Fit((Comp1_Comp2[:, 0], Comp1_Comp2[:, 1]),
+                 (dist_description_0, dist_description_1))
+
+    iform_contour = IFormContour(my_fit.mul_var_dist, Time_R, Time_SS, 1000)
+    
+    comp_coords = np.array(iform_contour.coordinates).T
+    coords = pca.inverse_transform(comp_coords, clip=True)
+    
+    ref = pd.read_csv(f"tests/testfiles/PCA_IFORM_buoy_{buoy_num}.csv")
+    
+    assert_allclose(coords, ref[["Hs", "T"]].values)
+    
+    
+    
+    
+    
---
+++ viroconcom/utility.py
@@ -0,0 +1,74 @@
+"""
+Utilities for contours.
+"""
+import numpy as np
+from sklearn.decomposition import PCA as skPCA
+
+
+class PCA():
+    """
+    Principal component analysis (PCA) for 2D wave data.
+    
+    Wraps sklearn PCA to ease use for wave data.
+    Similar to ESSC PCA.
+    https://github.com/WEC-Sim/WDRT/blob/master/WDRT/ESSC.py
+    
+    """
+    
+    def __init__(self, data):
+        """
+
+        Parameters
+        ----------
+        data : ndarray
+            The wave data with shape = (number of observations, 2).
+
+        """
+        pca = skPCA(n_components=2)
+        pca.fit(data)
+        self.components = pca.components_
+        self.transformed = self._transform(data)
+        
+    def _transform(self, data):
+        #  sklearn pca.transform normalizes before transforming 
+        # X_transformed = np.dot(X, self.components_.T)
+        # here we do not normalize
+        Comp1_Comp2 = np.dot(data, self.components.T)
+        
+        # shift = abs(min(Comp1_Comp2[:, 1])) + 0.1 # ESSC style shift
+        c2_min = min(Comp1_Comp2[:, 1])
+        if c2_min < 0:
+            self.shift = abs(c2_min) + 0.1  # Calculate shift
+        else:
+            self.shift = 0
+            
+        # Apply shift to Component 2 to make all values positive
+        Comp1_Comp2[:, 1] = Comp1_Comp2[:, 1] + self.shift
+        return Comp1_Comp2
+    
+    def inverse_transform(self, data, clip=True):
+        """
+        Returns the inverse pca of data.        
+
+        Parameters
+        ----------
+        data : ndarray
+            The data to inverse transform.
+        clip : boolean, optional
+            If True unphysical values(<0) are set to zero. The default is True.
+
+        Returns
+        -------
+        ndarray
+            The inverse transformed data.
+
+        """
+        dat = data.copy()
+        dat[:,1] = dat[:,1] - self.shift
+        inv_transformed_data = np.dot(dat, self.components)
+        if clip:
+            return np.clip(inv_transformed_data, 0, None)
+        else:
+            return inv_transformed_data
+
+        
