49a196ef53cdc7d6190904b49d55410ed2dd77ae
Andreas <a.haselsteiner@uni-bremen.de>
2020-09-28 11:23:38 +0200

    Format code nicely, add comments.

--- tests/test_contour_analysis.py
+++ tests/test_contour_analysis.py
@@ -11,6 +11,7 @@ from viroconcom.read_write import read_ecbenchmark_dataset, read_contour
 from viroconcom.contour_analysis import points_outside
 from viroconcom.plot import plot_contour
 
+
 class ContourAnalysisTest(unittest.TestCase):
 
     def tests_points_outside(self):
@@ -18,17 +19,16 @@ class ContourAnalysisTest(unittest.TestCase):
         Tests whether points_outside identifies the correct number of points
         outside the contour.
         """
-
+        # Load a metocean dataset an environmental contour.
         v, hs, label_v, label_hs = read_ecbenchmark_dataset('datasets/1year_dataset_D.txt')
-
         folder_name = 'contour-coordinates/'
         file_name_median = 'doe_john_years_25_median.txt'
         (contour_v, contour_hs) = read_contour(folder_name + file_name_median)
 
-
-
+        # Compute the points outside the contour.
         (v_outside, hs_outside, v_inside, hs_inside) = points_outside(contour_v, contour_hs, v, hs)
 
+        # Manually check the results, 5 points are outside.
         fig, ax = plt.subplots()
         ax.scatter(v_inside, hs_inside)
         ax.scatter(v_outside, hs_outside, c='r')
