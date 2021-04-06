380ffd4a4eb13b529da722268df3ecaa744a7002
kawindme <32780200+kawindme@users.noreply.github.com>
2021-04-06 11:51:09 +0200

    change PointsPerIntervalSlicer boundaries

--- tests/test_intervals.py
+++ tests/test_intervals.py
@@ -58,7 +58,7 @@ def woi_params_and_ref(request):
      [1.8333333333333333, 3.0999999999999996, 4.366666666666666],
      [[1.2, 1.5, 2.4], [2.5, 2.6, 3.1, 3.5, 3.6], [4.0, 5.0]],
      [[1.2, 2.466666666666667], [2.4666666666666663, 3.733333333333333],[3.733333333333333, 5.0]]),
-    ({"n_intervals" : 3, "min_n_points" : 3},
+    ({"n_intervals" : 3, "min_n_points" : 3, "min_n_intervals" : 2},
      [1.8333333333333333, 3.0999999999999996],
      [[1.2, 1.5, 2.4], [2.5, 2.6, 3.1, 3.5, 3.6]],
      [[1.2, 2.466666666666667], [2.4666666666666663, 3.733333333333333]]),
@@ -74,6 +74,10 @@ def woi_params_and_ref(request):
      [2.4, 3.6],
      [[1.2, 1.5, 2.4, 2.5, 2.6], [3.1, 3.5, 3.6, 4.0, 5.0]],
      [[1.2, 3.1], [3.1, 5.0]]),
+    ({"n_intervals" : 3, "min_n_points" : 3}, # should raise RuntimeError (too few intervals)
+     None,
+     None,
+     None),
     ])
 def noi_params_and_ref(request):
     params = request.param[0]
@@ -90,23 +94,23 @@ def noi_params_and_ref(request):
     ({"n_points" : 2, "min_n_points" : 1},
      [1.35, 2.45, 2.85, 3.55, 4.5],
      [[1.2, 1.5], [2.4, 2.5], [2.6, 3.1], [3.5, 3.6], [4.0, 5.0]],
-     [[1.2, 1.5], [2.4, 2.5], [2.6, 3.1], [3.5, 3.6], [4.0, 5.0]]),
+     [[1.2, 1.95], [1.95, 2.55], [2.55, 3.3], [3.3, 3.8], [3.8, 5.0]]),
     ({"n_points" : 3, "min_n_points" : 1},
      [1.2, 2.4, 3.1, 4.0],
      [[1.2], [1.5, 2.4, 2.5], [2.6, 3.1, 3.5], [3.6, 4.0, 5.0]],
-     [[1.2, 1.2], [1.5, 2.5], [2.6, 3.5], [3.6, 5.0]]),
+     [[1.2, 1.35], [1.35, 2.55], [2.55, 3.55], [3.55, 5.0]]),
     ({"n_points" : 3, "last_full": False, "min_n_points" : 1},
      [1.5, 2.6, 3.6, 5.0],
      [[1.2, 1.5, 2.4], [2.5, 2.6, 3.1], [3.5, 3.6, 4.0], [5.0]],
-     [[1.2, 2.4], [2.5, 3.1], [3.5, 4.0], [5.0, 5.0]]),
+     [[1.2, 2.45], [2.45, 3.3], [3.3, 4.5], [4.5, 5.0]]),
     ({"n_points" : 3,},
      [2.4, 3.1, 4.0],
      [[1.5, 2.4, 2.5], [2.6, 3.1, 3.5], [3.6, 4.0, 5.0]],
-     [[1.5, 2.5], [2.6, 3.5], [3.6, 5.0]]),
+     [[1.5, 2.55], [2.55, 3.55], [3.55, 5.0]]),
     ({"n_points" : 3, "center": np.mean, "min_n_points" : 1},
      [1.2, 2.1333333333333333, 3.0666666666666664, 4.2],
      [[1.2], [1.5, 2.4, 2.5], [2.6, 3.1, 3.5], [3.6, 4.0, 5.0]],
-     [[1.2, 1.2], [1.5, 2.5], [2.6, 3.5], [3.6, 5.0]]),
+     [[1.2, 1.35], [1.35, 2.55], [2.55, 3.55], [3.55, 5.0]]),
     ])
 def ppi_params_and_ref(request):
     params = request.param[0]
@@ -146,14 +150,18 @@ def test_number_of_intervals_slicer(test_data, noi_params_and_ref):
     ref_boundaries = noi_params_and_ref["boundaries"]
     slicer = NumberOfIntervalsSlicer(**params)
     
-    my_slices, my_centers, my_boundaries = slicer.slice_(test_data)
-    my_intervals = [test_data[slice_] for slice_ in my_slices]
-    np.testing.assert_almost_equal(my_centers, ref_centers)
-    assert len(my_intervals) == len(ref_intervals)
-    for i in range(len(my_intervals)):
-        np.testing.assert_array_equal(my_intervals[i], ref_intervals[i])
-    for i in range(len(my_boundaries)):
-        np.testing.assert_almost_equal(my_boundaries[i], ref_boundaries[i])
+    if ref_centers is None:
+        with pytest.raises(RuntimeError):
+            my_slices, my_centers, my_boundaries = slicer.slice_(test_data)
+    else:
+        my_slices, my_centers, my_boundaries = slicer.slice_(test_data)
+        my_intervals = [test_data[slice_] for slice_ in my_slices]
+        np.testing.assert_almost_equal(my_centers, ref_centers)
+        assert len(my_intervals) == len(ref_intervals)
+        for i in range(len(my_intervals)):
+            np.testing.assert_array_equal(my_intervals[i], ref_intervals[i])
+        for i in range(len(my_boundaries)):
+            np.testing.assert_almost_equal(my_boundaries[i], ref_boundaries[i])
         
         
 def test_points_per_interval_slicer(test_data, ppi_params_and_ref):
