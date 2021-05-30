c9b3b2c9598986b4c3d1da11346f2fa01abb713c
Andreas Haselsteiner <a.haselsteiner@uni-bremen.de>
2021-05-31 00:01:45 +0200

    Finish joint model example.

--- examples/compare_joint_models.py
+++ examples/compare_joint_models.py
@@ -2,6 +2,7 @@
 Fit two different  join distribution models to a wind-wave 
 dataset and compare their goodness of fits.
 """
+import numpy as np
 
 from virocon.plotting import plot_2D_isodensity, plot_dependence_functions, plot_marginal_quantiles
 import matplotlib.pyplot as plt
@@ -35,19 +36,27 @@ dist_descriptions2, fit_descriptions2, semantics2 = get_OMAE2020_V_Hs()
 model2 = GlobalHierarchicalModel(dist_descriptions2)
 
 # Fit the two models to the data (estimate their parameter values).
-model1.fit(data, fit_descriptions1)
+# For model 1, we need the variables in the order hs, v (instead of v, hs)
+v = data['wind speed (m/s)'].to_numpy()
+hs = data['significant wave height (m)'].to_numpy()
+hs_v = np.transpose(np.array([hs, v]))
+model1.fit(hs_v, fit_descriptions1)
 model2.fit(data, fit_descriptions2)
 
-print(model1)
-
 # Analyze the goodness of fit of the marginal distributions with QQ plots.
-fig, axs = plt.subplots(2, 2, figsize=[8, 8])
-plot_marginal_quantiles(model1, data, semantics=semantics1, axes=axs[0])
-plot_marginal_quantiles(model2, data, semantics=semantics2, axes=axs[1])
+fig, axs = plt.subplots(1, 2, figsize=[8, 4])
+plot_marginal_quantiles(model1, data, semantics=semantics1, axes=axs)
+fig.suptitle('DNVGL 2017 model')
+fig, axs = plt.subplots(1, 2, figsize=[8, 4])
+plot_marginal_quantiles(model2, data, semantics=semantics2, axes=axs)
+fig.suptitle('OMAE 2020 model')
 
-#titles = ['IFORM', 'ISORM', 'Direct sampling', 'Highest density']
-#for ax, title in zip(axs, titles):
-#    ax.set_title(title)
+# Analyze overall goodness of fit by plotting isodensity lines.
+fig, axs = plt.subplots(1, 2, figsize=[8, 4])
+plot_2D_isodensity(model1, hs_v, semantics1, swap_axis=True, ax=axs[0])
+plot_2D_isodensity(model2, data, semantics2, ax=axs[1])
+axs[0].set_title('DNVGL 2017 model')
+axs[1].set_title('OMAE 2020 model')
 
 plt.tight_layout()
 plt.show()
