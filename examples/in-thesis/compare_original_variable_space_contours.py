adfa230261942a4cd1645ca4f9ba331e615d2cac
Andreas Haselsteiner <a.haselsteiner@uni-bremen.de>
2021-07-30 00:54:52 +0200

    Plot AND and OR, only exceedance line.

--- examples/in-thesis/compare_original_variable_space_contours.py
+++ examples/in-thesis/compare_original_variable_space_contours.py
@@ -20,17 +20,33 @@ model.fit(data, fit_descriptions)
 
 # Compute the contours.
 alpha = 0.1
-n = 10000
+n = 1000
 sample = model.draw_sample(n)
-and_contour = AndContour(model, alpha, deg_step=1, sample=sample, allowed_error=0.01)
-or_contour = OrContour(model, alpha, deg_step=1, sample=sample, allowed_error=0.01)
-direct_sampling = DirectSamplingContour(model, alpha, sample=sample)
+and_contour = AndContour(model, alpha, deg_step=1, sample=sample, allowed_error=0.001)
+or_contour = OrContour(model, alpha, deg_step=1, sample=sample, allowed_error=0.001, lowest_theta=2)
+direct_sampling = DirectSamplingContour(model, alpha, sample=sample, deg_step=10)
 highest_density = HighestDensityContour(model, alpha, limits=[(0, 20), (0, 20)], deltas=0.02)
 
 # Plot the contours.
 fig, axs = plt.subplots(2, 2, figsize=[8, 8], sharex=True, sharey=True)
-plot_2D_contour(and_contour, sample=sample, ax=axs[0][0])
-plot_2D_contour(or_contour, sample=sample, ax=axs[0][1])
+axs[0][0].scatter(
+    sample[:, 0],
+    sample[:, 1],
+    c="k",
+    marker=".",
+    alpha=0.3,
+    rasterized=True,
+)
+axs[0][0].plot(and_contour.coordinates[0:-1,0], and_contour.coordinates[0:-1,1], c="#BB5566")
+axs[0][1].scatter(
+    sample[:, 0],
+    sample[:, 1],
+    c="k",
+    marker=".",
+    alpha=0.3,
+    rasterized=True,
+)
+axs[0][1].plot(or_contour.coordinates[0:-3,0], or_contour.coordinates[0:-3,1], c="#BB5566")
 plot_2D_contour(direct_sampling, sample=sample, ax=axs[1][0])
 plot_2D_contour(highest_density, sample=sample, ax=axs[1][1])
 titles = [['AND exceedance', 'OR exceedance'],['Angular exceedance', 'Isodensity exceedance']]
