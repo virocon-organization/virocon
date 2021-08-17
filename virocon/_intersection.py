f0edc0e1c9dd70af9db493b48fc731d47dc114a9
lenastroeer <57865209+lenastroeer@users.noreply.github.com>
2021-08-17 13:17:57 +0200

    solved in line emphasis in _intersection.py

--- virocon/_intersection.py
+++ virocon/_intersection.py
@@ -73,8 +73,8 @@ def intersection(x1, y1, x2, y2):
     Example:
     a, b = 1, 2
     phi = np.linspace(3, 10, 100)
-    x1 = a*phi - b*np.sin(phi)
-    y1 = a - b*np.cos(phi)
+    x1 = a \* phi - b \* np.sin(phi)
+    y1 = a - b \* np.cos(phi)
 
     x2=phi
     y2=np.sin(phi)+2
@@ -82,7 +82,7 @@ def intersection(x1, y1, x2, y2):
 
     plt.plot(x1,y1,c='r')
     plt.plot(x2,y2,c='g')
-    plt.plot(x,y,'*k')
+    plt.plot(x,y,'.k')
     plt.show()
 
     """
