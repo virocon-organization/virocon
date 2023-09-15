fc4119b06916e8b479b987939fc7120948bd8773
Andreas Haselsteiner <a.haselsteiner@uni-bremen.de>
2023-09-15 11:34:08 +0200

    Change EW script to script that compures contours for ALL Hs-Tz models.

---
+++ examples/hstz_contour_all_models.py
@@ -0,0 +1,153 @@
+"""
+Computes sea state contours using all available Hs-Tz joint model structures.
+"""
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from virocon import (
+    read_ec_benchmark_dataset,
+    variable_transform,
+    get_Windmeier_Hs_S,
+    get_EW_Hs_S,
+    get_OMAE2020_Hs_Tz,
+    get_DNVGL_Hs_Tz,
+    GlobalHierarchicalModel,
+    TransformedModel,
+    IFORMContour,
+    plot_2D_isodensity,
+    plot_2D_contour,
+)
+
+# Load sea state measurements.
+data_hs_tz = read_ec_benchmark_dataset("datasets/ec-benchmark_dataset_A_1year.txt")
+hs = data_hs_tz["significant wave height (m)"]
+tz = data_hs_tz["zero-up-crossing period (s)"]
+temp, steepness = variable_transform.hs_tz_to_hs_s(hs, tz)
+steepness.name = "steepness"
+data_hs_s = pd.concat([hs, steepness], axis=1)
+
+# Define the structure of Windmeier's originial EW model (see DOI: 10.26092/elib/2181).
+(
+    dist_descriptions,
+    fit_descriptions,
+    hs_tz_semantics,
+    transformations,
+) = get_Windmeier_Hs_S()
+windmeier_ew_model = GlobalHierarchicalModel(dist_descriptions)
+
+# Fit the model in Hs-S space.
+print("Fitting the model.")
+windmeier_ew_model.fit(data_hs_s, fit_descriptions)
+
+hs_s_semantics = {
+    "names": ["Significant wave height", "Steepness"],
+    "symbols": ["H_s", "S"],
+    "units": ["m", "-"],
+}
+
+# Transform the fitted model to Hs-Tz space.
+windmeier_t_model = TransformedModel(
+    windmeier_ew_model,
+    transformations["transform"],
+    transformations["inverse"],
+    transformations["jacobian"],
+    precision_factor=0.2,  # Use low precision to speed up test.
+    random_state=42,
+)
+
+# Compute a contour.
+s = (
+    "Computing contours. This will take 1-10 minutes because a Monte Carlo "
+    "method is used for evaluating the ICDF of the TransFormedModel."
+)
+print(s)
+tr = 1  # Return period in years.
+ts = 1  # Sea state duration in hours.
+alpha = 1 / (tr * 365.25 * 24 / ts)
+windmeier_model_contour = IFORMContour(windmeier_t_model, alpha, n_points=50)
+
+
+# Define the structure of the EW model which has a dependence function for scale
+# that evaluates to >0 at Hs=0 (the model was inspired by Windmeier's
+# original EW model that was presented in DOI: 10.26092/elib/2181).
+(
+    dist_descriptions,
+    fit_descriptions,
+    hs_tz_semantics,
+    transformations,
+) = get_EW_Hs_S()
+ew_model = GlobalHierarchicalModel(dist_descriptions)
+
+# Fit the model in Hs-S space.
+print("Fitting the model.")
+ew_model.fit(data_hs_s, fit_descriptions)
+
+hs_s_semantics = {
+    "names": ["Significant wave height", "Steepness"],
+    "symbols": ["H_s", "S"],
+    "units": ["m", "-"],
+}
+
+# Transform the fitted model to Hs-Tz space.
+ew_t_model = TransformedModel(
+    ew_model,
+    transformations["transform"],
+    transformations["inverse"],
+    transformations["jacobian"],
+    precision_factor=0.2,  # Use low precision to speed up test.
+    random_state=42,
+)
+
+ew_model_contour = IFORMContour(ew_t_model, alpha, n_points=50)
+
+
+# Compute a contour based on on a DNV model for comparison
+(
+    dnv_dist_descriptions,
+    dnv_fit_descriptions,
+    dnv_semantics,
+) = get_DNVGL_Hs_Tz()
+dnv_model = GlobalHierarchicalModel(dnv_dist_descriptions)
+dnv_model.fit(data_hs_tz, fit_descriptions=dnv_fit_descriptions)
+dnv_contour = IFORMContour(dnv_model, alpha)
+
+# Compute a contour based on on a OMAE2020 model for comparison
+(
+    omae2020_dist_descriptions,
+    omae2020_fit_descriptions,
+    omae2020_semantics,
+) = get_OMAE2020_Hs_Tz()
+omae2020_model = GlobalHierarchicalModel(omae2020_dist_descriptions)
+omae2020_model.fit(data_hs_tz, fit_descriptions=omae2020_fit_descriptions)
+omae2020_contour = IFORMContour(omae2020_model, alpha)
+
+# Plot the contours on top of the metocean data.
+fig, ax = plt.subplots(1, 1, figsize=[5, 5])
+plot_2D_contour(
+    dnv_contour, data_hs_tz, semantics=hs_tz_semantics, swap_axis=True, ax=ax
+)
+plot_2D_contour(
+    omae2020_contour, data_hs_tz, semantics=hs_tz_semantics, swap_axis=True, ax=ax
+)
+plot_2D_contour(
+    windmeier_model_contour,
+    data_hs_tz,
+    semantics=hs_tz_semantics,
+    swap_axis=True,
+    ax=ax,
+)
+plot_2D_contour(
+    ew_model_contour, data_hs_tz, semantics=hs_tz_semantics, swap_axis=True, ax=ax
+)
+ax.lines[1].set_color("orchid")
+ax.lines[2].set_color("blue")
+ax.lines[3].set_color("gray")
+ax.legend(
+    [ax.lines[0], ax.lines[1], ax.lines[2]],
+    ["DNV model", "OMAE2020 model", "Windmeier's EW model", "Nonzero EW model"],
+    ncol=1,
+    frameon=False,
+)
+ax.set_title(f"{tr}-year IFORM contour")
+
+plt.show()
